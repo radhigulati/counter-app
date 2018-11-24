cf56f38dd03cd4f67a52ac37ff2f36238fb5a275
Radhika Gulati <radhigulati@gmail.com>
2018-11-23 21:01:15 -0800

    updates

---
+++ src/components/counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  //   style = {
+  //     fontSize: 50,
+  //     fontWeight: "bold"
+  //   };
+
+  render() {
+    return (
+      <div>
+        {/* inline style would look like: <span style={{ fontSize: 30 }}  */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          <li />
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
